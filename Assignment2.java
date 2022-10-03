f133ec8b8a8a7ea39354f4cfb3f6cae670cb157e
GreenMatt413 <111987332+GreenMatt413@users.noreply.github.com>
2022-10-03 09:10:07 -0400

    Start top part of diamond

--- Assignment2.java
+++ Assignment2.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
+
 public class Assignment2 {
     public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+    int diameter, topOfDiamond;
+
+    System.out.println("Enter a number ");
+    diameter = scan.nextInt();
+
+    topOfDiamond = 1;
+    while(topOfDiamond<diameter);{
+    System.out.println ("D" + topOfDiamond);
+    topOfDiamond =+2;
+    }
+
+
     }
 }
