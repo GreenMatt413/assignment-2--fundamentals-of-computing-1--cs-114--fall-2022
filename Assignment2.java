7ad29b18aaa905c0087f9c8ea3576e40c39835db
GreenMatt413 <111987332+GreenMatt413@users.noreply.github.com>
2022-10-03 17:19:47 -0400

    fix topOfDiamond while loop

--- Assignment2.java
+++ Assignment2.java
@@ -4,17 +4,21 @@ public class Assignment2 {
     public static void main(String[] args) {
 
     Scanner scan = new Scanner(System.in);
-    int diameter, topOfDiamond;
+    int diameter, topOfDiamond, bottomOfDiamond;
 
     System.out.println("Enter a number ");
     diameter = scan.nextInt();
 
     topOfDiamond = 1;
-    while(topOfDiamond <= diameter);{
+    while(topOfDiamond <= diameter){
     System.out.println ("*" + topOfDiamond);
     topOfDiamond +=2;
     }
-
+    bottomOfDiamond = 1;
+    while(diameter >= bottomOfDiamond){
+    System.out.println ("*" + topOfDiamond);
+    diameter -=2;
+    }
     System.out.println("test to see if the program finishes");
     }
 }
