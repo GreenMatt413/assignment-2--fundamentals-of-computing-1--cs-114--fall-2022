ce428205d0e7c211f23a6cdda69d0082023adbb4
GreenMatt413 <111987332+GreenMatt413@users.noreply.github.com>
2022-10-04 18:41:30 -0400

    finish bottom half of odd and all of even (god help me)

--- Assignment2.java
+++ Assignment2.java
@@ -26,31 +26,52 @@ public class Assignment2 {
 
     topOfDiamond -=4; //sets topOfDiamond value equal to diameter
     bottomOfDiamond = 1;
+    numberOfSpaces = 2; //resets numberOfSpaces variable so that it start at +1 space
+    System.out.print(" ");//adds +1 space but only for the first bottom half row
     while(topOfDiamond >= bottomOfDiamond){
         for (int i=0; i<topOfDiamond; i++){
             System.out.print("*");
         }
-    topOfDiamond -=2; System.out.print("\n");
+    topOfDiamond -=2; numberOfSpaces +=2; System.out.print("\n");
+    for (int k=0; k<(numberOfSpaces/2);k++){
+        System.out.print(" ");
+    }
     }
     }
 
 
     else {
-    System.out.println("*");
+
     topOfDiamond = 2;
+    numberOfSpaces = diameter;
+    for (int f=0; f<(numberOfSpaces-1);f++){
+        System.out.print(" ");
+    }
+    System.out.println("* ");
+    for (int j=0; j<(numberOfSpaces-2);j++){
+        System.out.print(" ");
+    }
     while(topOfDiamond <= diameter){
         for (int i=0; i<topOfDiamond; i++){
-        System.out.print(" *");
+        System.out.print("* ");
         }
-        topOfDiamond +=2; System.out.print("\n");
+        topOfDiamond +=2; numberOfSpaces-=2; System.out.print("\n");
+    for (int k=0; k<((numberOfSpaces-2));k++){
+            System.out.print(" ");
+        }
     }
-    topOfDiamond -=4; //Does the same thing as the comment above
+    topOfDiamond -=4; //sets topOfDiamond value equal to diameter
     bottomOfDiamond = 2;
+    numberOfSpaces = 2;
+    System.out.print(" ");//adds +1 space but only for the first bottom half row
     while(topOfDiamond >= bottomOfDiamond){
         for (int i=0; i<topOfDiamond; i++){
             System.out.print(" *");
         }
-    topOfDiamond -=2; System.out.print("\n");
+    topOfDiamond -=2; numberOfSpaces +=2; System.out.print("\n");
+    for (int k=0; k<(numberOfSpaces-1);k++){
+        System.out.print(" ");
+    }
     }
     System.out.println("*");
     }
