3092849c87811a11707b0604f2c7595b888f5ca7
GreenMatt413 <111987332+GreenMatt413@users.noreply.github.com>
2022-10-04 17:42:25 -0400

    add code that lines up top half of odd diamonds

--- Assignment2.java
+++ Assignment2.java
@@ -4,24 +4,31 @@ public class Assignment2 {
     public static void main(String[] args) {
 
     Scanner scan = new Scanner(System.in);
-    int diameter, topOfDiamond, bottomOfDiamond;
+    int diameter, topOfDiamond, bottomOfDiamond, numberOfSpaces;
 
     System.out.println("Enter a number ");
     diameter = scan.nextInt();
-
     if (diameter % 2 >= 1) {
     topOfDiamond = 1;
+    numberOfSpaces = diameter;
+    for (int j=0; j<(numberOfSpaces/2);j++){
+        System.out.print(" ");
+    }
     while(topOfDiamond <= diameter){
         for (int i=0; i<topOfDiamond; i++){
-        System.out.print("*");
+            System.out.print("*");
         }
-        topOfDiamond +=2; System.out.print("\n");
+        topOfDiamond +=2; numberOfSpaces -=2; System.out.print("\n");
+    for (int k=0; k<(numberOfSpaces/2);k++){
+        System.out.print(" ");
     }
+    }
+
     topOfDiamond -=4; //sets topOfDiamond value equal to diameter
     bottomOfDiamond = 1;
     while(topOfDiamond >= bottomOfDiamond){
         for (int i=0; i<topOfDiamond; i++){
-    System.out.print("*");
+            System.out.print("*");
         }
     topOfDiamond -=2; System.out.print("\n");
     }
@@ -33,14 +40,19 @@ public class Assignment2 {
     topOfDiamond = 2;
     while(topOfDiamond <= diameter){
         for (int i=0; i<topOfDiamond; i++){
-        System.out.print("*");
+        System.out.print(" *");
         }
         topOfDiamond +=2; System.out.print("\n");
-
     }
-
-
-
+    topOfDiamond -=4; //Does the same thing as the comment above
+    bottomOfDiamond = 2;
+    while(topOfDiamond >= bottomOfDiamond){
+        for (int i=0; i<topOfDiamond; i++){
+            System.out.print(" *");
+        }
+    topOfDiamond -=2; System.out.print("\n");
+    }
+    System.out.println("*");
     }
 }
 }
